855a77f430a9036dc6eb38c3b97920ff34981857
Tim Steffens <tim.steffens@greenbone.net>
2023-12-12 09:45:58 +0100

    use unified naming

--- pkg/openSearch/esextension/compositeSources.go
+++ pkg/openSearch/esextension/compositeSources.go
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-package escomposite
+package esextensions
 
 // TermsSource represents a terms value source to composite aggregations, as described in
 // https://www.elastic.co/guide/en/elasticsearch/reference/7.17/search-aggregations-bucket-composite-aggregation.html#_terms
