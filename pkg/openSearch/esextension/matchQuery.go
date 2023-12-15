f8e997a6018503bc017eafb7328fbb6bf9ed2caa
Roxana Meixner <roxana.meixner@greenbone.net>
2023-12-15 14:26:30 +0100

    remove: remove unreachable code

--- pkg/openSearch/esextension/matchQuery.go
+++ pkg/openSearch/esextension/matchQuery.go
@@ -6,25 +6,13 @@ type MatchQuery struct {
 	Value interface{}
 }
 
-const useSimpleMap = true
-
 // Map returns a map representation of the MatchQuery, thus implementing the esquery.Mappable interface.
 // Used for serialization to JSON.
 func (mq *MatchQuery) Map() map[string]interface{} {
-	if useSimpleMap {
-		return map[string]interface{}{
-			"match": map[string]interface{}{
-				mq.Field: mq.Value,
-			},
-		}
-	} else {
-		return map[string]interface{}{
-			"match": map[string]interface{}{
-				mq.Field: map[string]interface{}{
-					"query": mq.Value,
-				},
-			},
-		}
+	return map[string]interface{}{
+		"match": map[string]interface{}{
+			mq.Field: mq.Value,
+		},
 	}
 }
 
