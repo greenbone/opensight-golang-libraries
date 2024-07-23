0a9f720aa436283050568420adb58764faae4d61
Endy Apina <39262413+endyApina@users.noreply.github.com>
2024-07-23 10:37:55 +0100

    change docs string for query and regenerate docs

--- pkg/query/paging/response.go
+++ pkg/query/paging/response.go
@@ -3,8 +3,8 @@ package paging
 // Response represents a response object containing information about pagination and total count of records.
 //   - PageIndex: The index of the page (starting from 0). This is required.
 //   - PageSize: The number of records per page. This is required.
-//   - TotalDisplayableResults: The total number of results that can be displayed. This is required.
-//   - TotalResults: The total number of results, including those that may not be displayed. This is optional and must not be set if the value does not differ from `TotalDisplayableResults`
+//   - TotalDisplayableResults: The total number of results that can be paginated. Due to database restrictions, in case of large number of results, some of the results cannot be retrieved. In such cases, this number will be lower than the `TotalResults`. This is required.
+//   - TotalResults: The total count of results as it exists in database, including those that may not be retrieved. This is optional and must not be set if the value does not differ from `TotalDisplayableResults`
 type Response struct {
 	PageIndex               int    `json:"index" binding:"required"`
 	PageSize                int    `json:"size" binding:"required"`
