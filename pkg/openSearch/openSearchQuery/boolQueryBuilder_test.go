b6019b41adc74b7a40a462a822696163f8288b74
Tim Steffens <tim.steffens@greenbone.net>
2024-08-23 13:33:58 +0200

    tidy test cases

--- pkg/openSearch/openSearchQuery/boolQueryBuilder_test.go
+++ pkg/openSearch/openSearchQuery/boolQueryBuilder_test.go
@@ -69,64 +69,85 @@ func TestBoolQueryBuilder(t *testing.T) {
 	})
 }
 
-func TestFilterQueryOperatorAndMultiValue(t *testing.T) {
+func TestFilterQueryOperatorAnd(t *testing.T) {
 	mixedTests := map[string]struct {
 		file     string
 		operator filter.CompareOperator
 		value    any
 	}{
-		"shouldReturnJsonForOperatorBeginsWithSingleValue": {
-			file:     "testdata/And/singleValue/BeginsWith.json",
-			operator: filter.CompareOperatorBeginsWith, value: 5,
-		},
-		"shouldReturnJsonForOperatorDoesNotBeginsWithSingleValue": {
-			file:     "testdata/And/singleValue/DoesNotBeginsWith.json",
-			operator: filter.CompareOperatorDoesNotBeginWith, value: "5",
-		},
-		"shouldReturnJsonForOperatorDoesNotBeginsWithMultiValue": {
-			file:     "testdata/And/multiValue/DoesNotBeginsWith.json",
-			operator: filter.CompareOperatorDoesNotBeginWith, value: []interface{}{"5", "6"},
-		},
+		// multi value
 		"shouldReturnJsonForOperatorBeginsWithMultiValue": {
 			file: "testdata/And/multiValue/BeginsWith.json", operator: filter.CompareOperatorBeginsWith, value: []interface{}{5, 6},
-		},
-		"shouldReturnJsonForOperatorContainsSingleValue": {
-			file: "testdata/And/singleValue/Contains.json", operator: filter.CompareOperatorContains, value: "test",
 		},
 		"shouldReturnJsonForOperatorContainsMultiValue": {
 			file: "testdata/And/multiValue/Contains.json", operator: filter.CompareOperatorContains,
 			value: []interface{}{"test1", "test2"},
 		},
-		"shouldReturnJsonForOperatorDoesNotContainSingleValue": {
-			file:     "testdata/And/singleValue/DoesNotContain.json",
-			operator: filter.CompareOperatorDoesNotContain, value: "test",
+		"shouldReturnJsonForOperatorDoesNotBeginsWithMultiValue": {
+			file:     "testdata/And/multiValue/DoesNotBeginsWith.json",
+			operator: filter.CompareOperatorDoesNotBeginWith, value: []interface{}{"5", "6"},
 		},
 		"shouldReturnJsonForOperatorDoesNotContainMultiValue": {
 			file:     "testdata/And/multiValue/DoesNotContain.json",
 			operator: filter.CompareOperatorDoesNotContain,
 			value:    []interface{}{"test1", "test2"},
 		},
-		"shouldReturnJsonForOperatorIsStringEqualToSingleValue": {
-			file:     "testdata/And/singleValue/IsStringEqualTo.json",
-			operator: filter.CompareOperatorIsStringEqualTo, value: 5,
-		},
 		"shouldReturnJsonForOperatorIsStringEqualToMultiValue": {
 			file:     "testdata/And/multiValue/IsStringEqualTo.json",
 			operator: filter.CompareOperatorIsStringEqualTo, value: []interface{}{5, 6},
-		},
-		"shouldReturnJsonForOperatorIsStringNotEqualToSingleValue": {
-			file:     "testdata/And/singleValue/IsStringNotEqualTo.json",
-			operator: filter.CompareOperatorIsStringNotEqualTo, value: 5,
 		},
 		"shouldReturnJsonForOperatorIsStringNotEqualToMultiValue": {
 			file:     "testdata/And/multiValue/IsStringNotEqualTo.json",
 			operator: filter.CompareOperatorIsStringNotEqualTo, value: []interface{}{5, 6},
 		},
+		// single value
+		"shouldReturnJsonForOperatorBeginsWithSingleValue": {
+			file:     "testdata/And/singleValue/BeginsWith.json",
+			operator: filter.CompareOperatorBeginsWith, value: 5,
+		},
+		"shouldReturnJsonForOperatorContainsSingleValue": {
+			file: "testdata/And/singleValue/Contains.json", operator: filter.CompareOperatorContains, value: "test",
+		},
+		"shouldReturnJsonForOperatorDoesNotBeginsWithSingleValue": {
+			file:     "testdata/And/singleValue/DoesNotBeginWith.json",
+			operator: filter.CompareOperatorDoesNotBeginWith, value: "5",
+		},
+		"shouldReturnJsonForOperatorDoesNotContainSingleValue": {
+			file:     "testdata/And/singleValue/DoesNotContain.json",
+			operator: filter.CompareOperatorDoesNotContain, value: "test",
+		},
+		"shouldReturnJsonForOperatorIsGreaterThan": {
+			file:     "testdata/And/singleValue/IsGreaterThan.json",
+			operator: filter.CompareOperatorIsGreaterThan, value: 5,
+		},
+		"shouldReturnJsonForOperatorIsGreaterThanOrEqualTo": {
+			file:     "testdata/And/singleValue/IsGreaterThanOrEqualTo.json",
+			operator: filter.CompareOperatorIsGreaterThanOrEqualTo, value: 5,
+		},
+		"shouldReturnJsonForOperatorIsLessThan": {
+			file:     "testdata/And/singleValue/IsLessThan.json",
+			operator: filter.CompareOperatorIsLessThan, value: 5,
+		},
+		"shouldReturnJsonForOperatorIsLessThanOrEqualTo": {
+			file:     "testdata/And/singleValue/IsLessThanOrEqualTo.json",
+			operator: filter.CompareOperatorIsLessThanOrEqualTo, value: 5,
+		},
+		"shouldReturnJsonForOperatorIsStringEqualToSingleValue": {
+			file:     "testdata/And/singleValue/IsStringEqualTo.json",
+			operator: filter.CompareOperatorIsStringEqualTo, value: "5",
+		},
+		"shouldReturnJsonForOperatorIsStringNotEqualToSingleValue": {
+			file:     "testdata/And/singleValue/IsStringNotEqualTo.json",
+			operator: filter.CompareOperatorIsStringNotEqualTo, value: "5",
+		},
 	}
 	for name, tc := range mixedTests {
 		t.Run(name, func(t *testing.T) {
+			// given
 			query := testBoolQueryBuilderWrapper{}
 			query.BoolQueryBuilder = NewBoolQueryBuilder(&querySettings)
+
+			// when
 			err := query.AddFilterRequest(&filter.Request{
 				Operator: filter.LogicOperatorAnd,
 				Fields: []filter.RequestField{
@@ -137,6 +158,8 @@ func TestFilterQueryOperatorAndMultiValue(t *testing.T) {
 					},
 				},
 			})
+
+			// then
 			require.NoError(t, err)
 			json, err := query.toJson()
 			require.NoError(t, err)
@@ -148,109 +171,81 @@ func TestFilterQueryOperatorAndMultiValue(t *testing.T) {
 	}
 }
 
-func TestFilterQueryOperatorAndSingleValue(t *testing.T) {
-	singleValueTests := map[string]struct {
-		file     string
-		operator filter.CompareOperator
-	}{
-		"shouldReturnJsonForOperatorIsGreaterThan": {
-			file:     "testdata/And/singleValue/IsGreaterThan.json",
-			operator: filter.CompareOperatorIsGreaterThan,
-		},
-		"shouldReturnJsonForOperatorIsGreaterThanOrEqualTo": {
-			file:     "testdata/And/singleValue/IsGreaterThanOrEqualTo.json",
-			operator: filter.CompareOperatorIsGreaterThanOrEqualTo,
-		},
-		"shouldReturnJsonForOperatorIsLessThan": {
-			file:     "testdata/And/singleValue/IsLessThan.json",
-			operator: filter.CompareOperatorIsLessThan,
-		},
-		"shouldReturnJsonForOperatorIsLessThanOrEqualTo": {
-			file:     "testdata/And/singleValue/IsLessThanOrEqualTo.json",
-			operator: filter.CompareOperatorIsLessThanOrEqualTo,
-		},
-	}
-	for name, tc := range singleValueTests {
-		t.Run(name, func(t *testing.T) {
-			query := testBoolQueryBuilderWrapper{}
-			query.BoolQueryBuilder = NewBoolQueryBuilder(&querySettings)
-			err := query.AddFilterRequest(&filter.Request{
-				Operator: filter.LogicOperatorAnd,
-				Fields: []filter.RequestField{
-					{
-						Name:     "testName",
-						Operator: tc.operator,
-						Value:    5,
-					},
-				},
-			})
-			require.NoError(t, err)
-			json, err := query.toJson()
-			require.NoError(t, err)
-
-			assert.JSONEq(t, testFolder.NewTestFolder().
-				GetContent(t, tc.file), json)
-		})
-	}
-}
-
 func TestFilterQueryOperatorOrMultiValue(t *testing.T) {
 	mixedTests := map[string]struct {
 		file     string
 		operator filter.CompareOperator
 		value    any
 	}{
-		"shouldReturnJsonForOperatorBeginsWithSingleValue": {
-			file: "testdata/Or/singleValue/BeginsWith.json", operator: filter.CompareOperatorBeginsWith, value: 5,
-		},
+		// multi value
 		"shouldReturnJsonForOperatorBeginsWithMultiValue": {
 			file: "testdata/Or/multiValue/BeginsWith.json", operator: filter.CompareOperatorBeginsWith, value: []interface{}{5, 6},
-		},
-		"shouldReturnJsonForOperatorContainsSingleValue": {
-			file: "testdata/Or/singleValue/Contains.json", operator: filter.CompareOperatorContains, value: "test",
 		},
 		"shouldReturnJsonForOperatorContainsMultiValue": {
 			file: "testdata/Or/multiValue/Contains.json", operator: filter.CompareOperatorContains,
 			value: []interface{}{"test1", "test2"},
-		},
-		"shouldReturnJsonForOperatorDoesNotContainSingleValue": {
-			file:     "testdata/Or/singleValue/DoesNotContain.json",
-			operator: filter.CompareOperatorDoesNotContain, value: "test1",
 		},
 		"shouldReturnJsonForOperatorDoesNotContainMultiValue": {
 			file:     "testdata/Or/multiValue/DoesNotContain.json",
 			operator: filter.CompareOperatorDoesNotContain,
 			value:    []interface{}{"test1", "test2"},
 		},
-		"shouldReturnJsonForOperatorIsStringEqualToSingleValue": {
-			file:     "testdata/Or/singleValue/IsStringEqualTo.json",
-			operator: filter.CompareOperatorIsStringEqualTo, value: "test",
+		"shouldReturnJsonForOperatorIsEqualToMultiValue": {
+			file: "testdata/Or/multiValue/IsEqualTo.json", operator: filter.CompareOperatorIsEqualTo, value: []interface{}{5, 6},
+		},
+		"shouldReturnJsonForOperatorIsNotEqualToMultiValue": {
+			file:     "testdata/Or/multiValue/IsNotEqualTo.json",
+			operator: filter.CompareOperatorIsNotEqualTo, value: []interface{}{5, 6},
 		},
 		"shouldReturnJsonForOperatorIsStringEqualToMultiValue": {
 			file:     "testdata/Or/multiValue/IsStringEqualTo.json",
 			operator: filter.CompareOperatorIsStringEqualTo, value: []interface{}{5, 6},
 		},
-		"shouldReturnJsonForOperatorIsStringNotEqualToSingleValue": {
-			file:     "testdata/Or/singleValue/IsStringNotEqualTo.json",
-			operator: filter.CompareOperatorIsStringNotEqualTo, value: 5,
-		},
 		"shouldReturnJsonForOperatorIsStringNotEqualToMultiValue": {
 			file:     "testdata/Or/multiValue/IsStringNotEqualTo.json",
 			operator: filter.CompareOperatorIsStringNotEqualTo, value: []interface{}{5, 6},
 		},
+		// single value
+		"shouldReturnJsonForOperatorBeginsWithSingleValue": {
+			file: "testdata/Or/singleValue/BeginsWith.json", operator: filter.CompareOperatorBeginsWith, value: 5,
+		},
+		"shouldReturnJsonForOperatorContainsSingleValue": {
+			file: "testdata/Or/singleValue/Contains.json", operator: filter.CompareOperatorContains, value: "test",
+		},
+		"shouldReturnJsonForOperatorDoesNotContainSingleValue": {
+			file:     "testdata/Or/singleValue/DoesNotContain.json",
+			operator: filter.CompareOperatorDoesNotContain, value: "test1",
+		},
 		"shouldReturnJsonForOperatorIsEqualToSingleValue": {
 			file: "testdata/Or/singleValue/IsEqualTo.json", operator: filter.CompareOperatorIsEqualTo, value: "test",
 		},
-		"shouldReturnJsonForOperatorIsEqualToMultiValue": {
-			file: "testdata/Or/multiValue/IsEqualTo.json", operator: filter.CompareOperatorIsEqualTo, value: []interface{}{5, 6},
+		"shouldReturnJsonForOperatorIsGreaterThan": {
+			file:     "testdata/Or/singleValue/IsGreaterThan.json",
+			operator: filter.CompareOperatorIsGreaterThan, value: 5,
+		},
+		"shouldReturnJsonForOperatorIsGreaterThanOrEqualTo": {
+			file:     "testdata/Or/singleValue/IsGreaterThanOrEqualTo.json",
+			operator: filter.CompareOperatorIsGreaterThanOrEqualTo, value: 5,
+		},
+		"shouldReturnJsonForOperatorIsLessThan": {
+			file:     "testdata/Or/singleValue/IsLessThan.json",
+			operator: filter.CompareOperatorIsLessThan, value: 5,
+		},
+		"shouldReturnJsonForOperatorIsLessThanOrEqualTo": {
+			file:     "testdata/Or/singleValue/IsLessThanOrEqualTo.json",
+			operator: filter.CompareOperatorIsLessThanOrEqualTo, value: 5,
 		},
 		"shouldReturnJsonForOperatorIsNotEqualToSingleValue": {
 			file:     "testdata/Or/singleValue/IsNotEqualTo.json",
 			operator: filter.CompareOperatorIsNotEqualTo, value: 5,
 		},
-		"shouldReturnJsonForOperatorIsNotEqualToMultiValue": {
-			file:     "testdata/Or/multiValue/IsNotEqualTo.json",
-			operator: filter.CompareOperatorIsNotEqualTo, value: []interface{}{5, 6},
+		"shouldReturnJsonForOperatorIsStringEqualToSingleValue": {
+			file:     "testdata/Or/singleValue/IsStringEqualTo.json",
+			operator: filter.CompareOperatorIsStringEqualTo, value: "test",
+		},
+		"shouldReturnJsonForOperatorIsStringNotEqualToSingleValue": {
+			file:     "testdata/Or/singleValue/IsStringNotEqualTo.json",
+			operator: filter.CompareOperatorIsStringNotEqualTo, value: 5,
 		},
 	}
 	for name, tc := range mixedTests {
@@ -264,52 +259,6 @@ func TestFilterQueryOperatorOrMultiValue(t *testing.T) {
 						Name:     "testName",
 						Operator: tc.operator,
 						Value:    tc.value,
-					},
-				},
-			})
-			require.NoError(t, err)
-			json, err := query.toJson()
-			require.NoError(t, err)
-
-			assert.JSONEq(t, testFolder.NewTestFolder().
-				GetContent(t, tc.file), json)
-		})
-	}
-}
-
-func TestFilterQueryOperatorOrSingleValue(t *testing.T) {
-	singleValueTests := map[string]struct {
-		file     string
-		operator filter.CompareOperator
-	}{
-		"shouldReturnJsonForOperatorIsGreaterThan": {
-			file:     "testdata/And/singleValue/IsGreaterThan.json",
-			operator: filter.CompareOperatorIsGreaterThan,
-		},
-		"shouldReturnJsonForOperatorIsGreaterThanOrEqualTo": {
-			file:     "testdata/And/singleValue/IsGreaterThanOrEqualTo.json",
-			operator: filter.CompareOperatorIsGreaterThanOrEqualTo,
-		},
-		"shouldReturnJsonForOperatorIsLessThan": {
-			file:     "testdata/And/singleValue/IsLessThan.json",
-			operator: filter.CompareOperatorIsLessThan,
-		},
-		"shouldReturnJsonForOperatorIsLessThanOrEqualTo": {
-			file:     "testdata/And/singleValue/IsLessThanOrEqualTo.json",
-			operator: filter.CompareOperatorIsLessThanOrEqualTo,
-		},
-	}
-	for name, tc := range singleValueTests {
-		t.Run(name, func(t *testing.T) {
-			query := testBoolQueryBuilderWrapper{}
-			query.BoolQueryBuilder = NewBoolQueryBuilder(&querySettings)
-			err := query.AddFilterRequest(&filter.Request{
-				Operator: filter.LogicOperatorAnd,
-				Fields: []filter.RequestField{
-					{
-						Name:     "testName",
-						Operator: tc.operator,
-						Value:    5,
 					},
 				},
 			})
