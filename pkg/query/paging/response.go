252e3a32303c5f495792a6428272682a0b74eee4
Endy Apina <39262413+endyApina@users.noreply.github.com>
2024-08-27 11:09:02 +0100

    change: provide current viewable paging results (#94)

--- pkg/query/paging/response.go
+++ pkg/query/paging/response.go
@@ -5,22 +5,46 @@
 package paging
 
 // Response represents a response object containing information about pagination and total count of records.
-//   - PageIndex: The index of the page (starting from 0).
-//   - PageSize: The number of records per page.
+//   - PageIndex: The index of the page (starting from 0). This is required.
+//   - PageSize: The number of records per page. This is required.
+//   - TotalDisplayableResults: The total number of results that can be paginated. Due to database restrictions, in case of large number of results, some of the results cannot be retrieved. In such cases, this number will be lower than the `TotalResults`. This is required.
+//   - TotalResults: The total count of results as it exists in database, including those that may not be retrieved. This is optional and must not be set if the value does not differ from `TotalDisplayableResults`
 type Response struct {
-	PageIndex     int    `json:"index"`
-	PageSize      int    `json:"size"`
-	TotalRowCount uint64 `json:"total"`
+	PageIndex               int    `json:"index" binding:"required"`
+	PageSize                int    `json:"size" binding:"required"`
+	TotalDisplayableResults uint64 `json:"totalDisplayableResults" binding:"required"`
+	TotalResults            uint64 `json:"totalResults,omitempty"`
 }
 
-func NewResponse(request *Request, totalRowCount uint64) *Response {
+func NewResponse(request *Request, totalDisplayableResults uint64) *Response {
 	if request == nil {
 		return nil
 	}
 
 	return &Response{
-		PageIndex:     request.PageIndex,
-		PageSize:      request.PageSize,
-		TotalRowCount: totalRowCount,
+		PageIndex:               request.PageIndex,
+		PageSize:                request.PageSize,
+		TotalDisplayableResults: totalDisplayableResults,
+	}
+}
+
+func NewResponseWithTotalResults(request *Request, totalResults, resultLimit uint64) *Response {
+	if request == nil {
+		return nil
+	}
+
+	if totalResults > resultLimit {
+		return &Response{
+			PageIndex:               request.PageIndex,
+			PageSize:                request.PageSize,
+			TotalDisplayableResults: resultLimit,
+			TotalResults:            totalResults,
+		}
+	}
+
+	return &Response{
+		PageIndex:               request.PageIndex,
+		PageSize:                request.PageSize,
+		TotalDisplayableResults: totalResults,
 	}
 }
