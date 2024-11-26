7fb5ee4d099d6762a44934576cc178973b2007c0
HollererJ <j.hollerer@amergy.com>
2024-11-26 19:48:26 +0100

    Added: add a count for an opensearch query - without the need to get the content

---
+++ pkg/openSearch/openSearchClient/count.go
@@ -0,0 +1,55 @@
+package openSearchClient
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+
+	"github.com/opensearch-project/opensearch-go/v4"
+	"github.com/opensearch-project/opensearch-go/v4/opensearchapi"
+)
+
+type CountReq struct {
+	Indices []string
+	Body    io.Reader
+	Header  http.Header
+	Params  map[string]string
+}
+
+// GetRequest returns the *http.Request that gets executed by the client
+func (r CountReq) GetRequest() (*http.Request, error) {
+	if r.Params == nil {
+		r.Params = make(map[string]string)
+	}
+	var path string
+	if len(r.Indices) > 0 {
+		path = fmt.Sprintf("/%s/_search", strings.Join(r.Indices, ","))
+	} else {
+		path = "/_search"
+	}
+
+	return opensearch.BuildRequest(
+		"GET",
+		path,
+		r.Body,
+		r.Params,
+		r.Header,
+	)
+}
+
+type CountResp struct {
+	Count  int64      `json:"count"`
+	Shards ShardStats `json:"_shards"`
+}
+
+type ShardStats struct {
+	Total      int `json:"total"`
+	Successful int `json:"successful"`
+	Skipped    int `json:"skipped"`
+	Failed     int `json:"failed"`
+}
+
+func (r CountResp) Inspect() opensearchapi.Inspect {
+	return opensearchapi.Inspect{}
+}
