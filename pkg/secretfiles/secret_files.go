d6f1c37f1e6b8cf44224d9b0bf194d4874ead6bd
mgoetzegb <marius.goetze@greenbone.net>
2024-11-27 16:34:40 +0100

    add: add package to read secrets from files (#132)
    Added function
    `secretfiles.Read` reads the value from the passed environment variable
    (name) and loads it into a string.
    
    ## Why
    
    This is a common scenario when working with Docker Secrets and is needed
    is multiple services.

---
+++ pkg/secretfiles/secret_files.go
@@ -0,0 +1,38 @@
+// SPDX-FileCopyrightText: 2024 Greenbone AG <https://greenbone.net>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+// Package secretfiles eases the parsing of secret files into a string.
+// This is a common scenario when working with docker secrets, where the
+// path to the secret is usually stored in an environment variable <SECRET>_FILE
+// containing a path to a secret stored in the container filesystem.
+package secretfiles
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+)
+
+// ReadSecret reads the file path from the given environment variable
+// and writes the content of this file into the target.
+// Surrounding whitespaces are truncated.
+func ReadSecret(envVar string, target *string) error {
+	if target == nil {
+		return errors.New("can't set target value, as it is nil")
+	}
+	path := os.Getenv(envVar)
+	if path != "" {
+		content, err := os.ReadFile(path)
+		if err != nil {
+			return fmt.Errorf("failed to read secret from file: %w", err)
+		}
+		secret := strings.TrimSpace(string(content))
+		if secret == "" {
+			return fmt.Errorf("secret in file %s is empty", path)
+		}
+		*target = strings.TrimSpace(string(content))
+	}
+	return nil
+}
