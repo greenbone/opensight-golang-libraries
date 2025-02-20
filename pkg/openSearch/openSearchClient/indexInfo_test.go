368625e38bedaa3a223a8c040a43d72035b3037d
HollererJ <j.hollerer@amergy.com>
2025-02-20 12:36:57 +0100

    Added: new functions to get the free disk space

---
+++ pkg/openSearch/openSearchClient/indexInfo_test.go
@@ -0,0 +1,91 @@
+package openSearchClient
+
+import (
+	"github.com/opensearch-project/opensearch-go/v4/opensearchapi"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestConvertToIndexInfo(t *testing.T) {
+	cases := []struct {
+		name     string
+		input    []opensearchapi.CatIndexResp
+		expected []IndexInfo
+	}{
+		{
+			name: "converts single index",
+			input: []opensearchapi.CatIndexResp{
+				{Index: "index1", CreationDate: 1625097600},
+			},
+			expected: []IndexInfo{
+				{Name: "index1", CreationDate: 1625097600},
+			},
+		},
+		{
+			name: "converts multiple indices",
+			input: []opensearchapi.CatIndexResp{
+				{Index: "index1", CreationDate: 1625097600},
+				{Index: "index2", CreationDate: 1625184000},
+			},
+			expected: []IndexInfo{
+				{Name: "index1", CreationDate: 1625097600},
+				{Name: "index2", CreationDate: 1625184000},
+			},
+		},
+		{
+			name:     "converts empty slice",
+			input:    []opensearchapi.CatIndexResp{},
+			expected: []IndexInfo{},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			result := ConvertToIndexInfo(tc.input)
+			assert.Equal(t, tc.expected, result)
+		})
+	}
+}
+
+func TestSortIndexInfoByCreationDate(t *testing.T) {
+	cases := []struct {
+		name     string
+		input    []IndexInfo
+		expected []IndexInfo
+	}{
+		{
+			name: "sorts indices by creation date",
+			input: []IndexInfo{
+				{Name: "index2", CreationDate: 1625184000},
+				{Name: "index1", CreationDate: 1625097600},
+			},
+			expected: []IndexInfo{
+				{Name: "index1", CreationDate: 1625097600},
+				{Name: "index2", CreationDate: 1625184000},
+			},
+		},
+		{
+			name: "handles already sorted indices",
+			input: []IndexInfo{
+				{Name: "index1", CreationDate: 1625097600},
+				{Name: "index2", CreationDate: 1625184000},
+			},
+			expected: []IndexInfo{
+				{Name: "index1", CreationDate: 1625097600},
+				{Name: "index2", CreationDate: 1625184000},
+			},
+		},
+		{
+			name:     "handles empty slice",
+			input:    []IndexInfo{},
+			expected: []IndexInfo{},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			result := SortIndexInfoByCreationDate(tc.input)
+			assert.Equal(t, tc.expected, result)
+		})
+	}
+}
