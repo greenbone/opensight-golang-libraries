2599b6d5b4dd163a945bea7a6234aab2a4cd4933
Endy Apina <apinaendy@gmail.com>
2025-02-27 17:45:34 +0100

    sync changes

--- pkg/query/filter/type_enum.go
+++ pkg/query/filter/type_enum.go
@@ -179,6 +179,8 @@ const (
 	CompareOperatorIsGreaterThanOrEqualToRating CompareOperator = "isGreaterThanOrEqualToRating"
 	// CompareOperatorIsLessThanOrEqualToRating is a CompareOperator of type isLessThanOrEqualToRating.
 	CompareOperatorIsLessThanOrEqualToRating CompareOperator = "isLessThanOrEqualToRating"
+	// CompareOperatorDateRange is a CompareOperator of type dateRange.
+	CompareOperatorDateRange CompareOperator = "dateRange"
 )
 
 var ErrInvalidCompareOperator = fmt.Errorf("not a valid CompareOperator, try [%s]", strings.Join(_CompareOperatorNames, ", "))
@@ -210,6 +212,7 @@ var _CompareOperatorNames = []string{
 	string(CompareOperatorIsLessThanRating),
 	string(CompareOperatorIsGreaterThanOrEqualToRating),
 	string(CompareOperatorIsLessThanOrEqualToRating),
+	string(CompareOperatorDateRange),
 }
 
 // CompareOperatorNames returns a list of possible string values of CompareOperator.
@@ -258,6 +261,7 @@ var _CompareOperatorValue = map[string]CompareOperator{
 	"isLessThanRating":               CompareOperatorIsLessThanRating,
 	"isGreaterThanOrEqualToRating":   CompareOperatorIsGreaterThanOrEqualToRating,
 	"isLessThanOrEqualToRating":      CompareOperatorIsLessThanOrEqualToRating,
+	"dateRange":                      CompareOperatorDateRange,
 }
 
 // ParseCompareOperator attempts to convert a string to a CompareOperator.
