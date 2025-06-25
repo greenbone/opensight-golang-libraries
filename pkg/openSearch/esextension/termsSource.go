e2be619fa3e1fba09181125ad83919afb480d3b9
HollererJ <external-johannes.hollerer@greenbone.net>
2025-06-25 15:37:53 +0200

    Added: missing bucket option added for fields which can have null values (#212)

--- pkg/openSearch/esextension/termsSource.go
+++ pkg/openSearch/esextension/termsSource.go
@@ -6,9 +6,10 @@ package esextensions
 
 // TermsSource represents a terms value source in composite aggregations.
 type TermsSource struct {
-	name  string
-	field string
-	order string // Add order field for sorting
+	name          string
+	field         string
+	order         string // Add order field for sorting
+	missingBucket bool
 }
 
 // Terms creates a new TermsSource.
@@ -17,10 +18,17 @@ type TermsSource struct {
 // field: The name of the field referenced.
 func Terms(name string, field string) *TermsSource {
 	return &TermsSource{
-		name:  name,
-		field: field,
-		order: "asc", // Default order is ascending
+		name:          name,
+		field:         field,
+		order:         "asc", // Default order is ascending
+		missingBucket: false,
 	}
+}
+
+// MissingBucket sets the missing_bucket flag to true in the TermsSource.
+func (t *TermsSource) MissingBucket() *TermsSource {
+	t.missingBucket = true
+	return t
 }
 
 // Order sets the sorting order for the TermsSource.
@@ -38,6 +46,10 @@ func (t *TermsSource) Map() map[string]interface{} {
 
 	if t.order != "" {
 		termsMap["order"] = t.order
+	}
+
+	if t.missingBucket {
+		termsMap["missing_bucket"] = true
 	}
 
 	return map[string]interface{}{
