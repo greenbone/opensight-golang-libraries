b1dc4285ac7fd7b0160b657cb9a26df66df0ee36
Marek Dalewski <marek.dalewski@stxnext.io>
2025-11-25 13:52:04 +0100

    change: fix failing tests in dbcrypt package

--- pkg/dbcrypt/dbcipher_test.go
+++ pkg/dbcrypt/dbcipher_test.go
@@ -113,7 +113,7 @@ func TestCipherCreationFailure(t *testing.T) {
 				Password:     "encryption-password",
 				PasswordSalt: "encryption-password-salt-0123456",
 			},
-			errorShouldContain: "invalid db cipher version",
+			errorShouldContain: "cipher version \"unknown\" not found",
 		},
 		{
 			name: "empty-password",
