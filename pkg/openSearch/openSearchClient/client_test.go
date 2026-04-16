637c76b2ddf65c2f1ce572bae6b4e5415663e0d2
Marius Goetze <marius.goetze@greenbone.net>
2026-04-16 20:16:53 +0200

    test: remove`testcontainers`
    I slows down tests and has a lot of dependencies which create a lot of noise during dependabot updates.

--- pkg/openSearch/openSearchClient/client_test.go
+++ pkg/openSearch/openSearchClient/client_test.go
@@ -6,8 +6,10 @@ package openSearchClient
 
 import (
 	"context"
+	"crypto/rand"
 	"encoding/json"
 	"io"
+	"strings"
 	"testing"
 	"time"
 
@@ -18,8 +20,6 @@ import (
 
 	"github.com/stretchr/testify/assert"
 )
-
-const indexName = "test"
 
 var (
 	folder         = testFolder.NewTestFolder()
@@ -44,11 +44,13 @@ func (v *Vulnerability) SetId(id string) {
 }
 
 func TestClient(t *testing.T) {
+	t.Parallel()
+
 	type testCase struct {
-		testFunc func(t *testing.T, client *Client, iFunc *IndexFunction)
+		testFunc func(t *testing.T, client *Client, iFunc *IndexFunction, indexName string)
 	}
 	tcs := map[string]testCase{
-		"TestBulkUpdate": {func(t *testing.T, client *Client, iFunc *IndexFunction) {
+		"TestBulkUpdate": {func(t *testing.T, client *Client, iFunc *IndexFunction, indexName string) {
 			// given
 			bulkRequest, err := SerializeDocumentsForBulkUpdate(indexName, []*Vulnerability{&aVulnerability})
 			require.NoError(t, err)
@@ -63,15 +65,15 @@ func TestClient(t *testing.T) {
 			require.NoError(t, err)
 
 			require.EventuallyWithT(t, func(c *assert.CollectT) {
-				searchResponse := searchAllVulnerabilities(c, client)
+				searchResponse := searchAllVulnerabilities(c, client, indexName)
 				assert.Equal(c, uint(1), searchResponse.Hits.Total.Value)
 				assert.Equal(c, 1, len(searchResponse.GetResults()))
 				assert.Equal(c, aVulnerability, *searchResponse.GetResults()[0])
 			}, 10*time.Second, 500*time.Millisecond)
 		}},
-		"TestUpdate": {func(t *testing.T, client *Client, _ *IndexFunction) {
+		"TestUpdate": {func(t *testing.T, client *Client, _ *IndexFunction, indexName string) {
 			// given
-			createDataInIndex(t, client, []*Vulnerability{&aVulnerability}, 1)
+			createDataInIndex(t, client, indexName, []*Vulnerability{&aVulnerability}, 1)
 			updateRequest := `{
 			  "query": {
 				"match_all": {}
@@ -92,16 +94,16 @@ func TestClient(t *testing.T) {
 			require.NoError(t, err)
 			log.Debug().Msgf("updateResponse: %s", string(updateResponse))
 			require.EventuallyWithT(t, func(c *assert.CollectT) {
-				searchResponse := searchAllVulnerabilities(c, client)
+				searchResponse := searchAllVulnerabilities(c, client, indexName)
 				assert.Equal(c, uint(1), searchResponse.Hits.Total.Value)
 				assert.Equal(c, 1, len(searchResponse.GetResults()))
 				assert.Equal(c, "This is the new name", searchResponse.GetResults()[0].Name)
 				assert.Equal(c, aVulnerability.Oid, searchResponse.GetResults()[0].Oid)
 			}, 10*time.Second, 500*time.Millisecond)
 		}},
-		"TestAsyncDeleteByQuery": {func(t *testing.T, client *Client, _ *IndexFunction) {
+		"TestAsyncDeleteByQuery": {func(t *testing.T, client *Client, _ *IndexFunction, indexName string) {
 			// given
-			createDataInIndex(t, client, []*Vulnerability{&aVulnerability}, 1)
+			createDataInIndex(t, client, indexName, []*Vulnerability{&aVulnerability}, 1)
 
 			// when
 			deleteQuery := `{"query":{"bool":{"filter":[{"term":{"oid":{"value":"1.3.6.1.4.1.25623.1.0.117842"}}}]}}}`
@@ -110,14 +112,14 @@ func TestClient(t *testing.T) {
 			// then
 			require.NoError(t, err)
 			require.EventuallyWithT(t, func(c *assert.CollectT) {
-				searchResponse := searchAllVulnerabilities(c, client)
+				searchResponse := searchAllVulnerabilities(c, client, indexName)
 				assert.Equal(c, uint(0), searchResponse.Hits.Total.Value)
 				assert.Equal(c, 0, len(searchResponse.GetResults()))
 			}, 10*time.Second, 500*time.Millisecond)
 		}},
-		"TestDeleteByQuery": {func(t *testing.T, client *Client, _ *IndexFunction) {
+		"TestDeleteByQuery": {func(t *testing.T, client *Client, _ *IndexFunction, indexName string) {
 			// given
-			createDataInIndex(t, client, []*Vulnerability{&aVulnerability}, 1)
+			createDataInIndex(t, client, indexName, []*Vulnerability{&aVulnerability}, 1)
 
 			// when
 			deleteQuery := `{"query":{"bool":{"filter":[{"term":{"oid":{"value":"1.3.6.1.4.1.25623.1.0.117842"}}}]}}}`
@@ -126,14 +128,14 @@ func TestClient(t *testing.T) {
 			// then
 			require.NoError(t, err)
 			require.EventuallyWithT(t, func(c *assert.CollectT) {
-				searchResponse := searchAllVulnerabilities(c, client)
+				searchResponse := searchAllVulnerabilities(c, client, indexName)
 				assert.Equal(c, uint(0), searchResponse.Hits.Total.Value)
 				assert.Equal(c, 0, len(searchResponse.GetResults()))
 			}, 10*time.Second, 500*time.Millisecond)
 		}},
-		"TestSearch": {func(t *testing.T, client *Client, _ *IndexFunction) {
+		"TestSearch": {func(t *testing.T, client *Client, _ *IndexFunction, indexName string) {
 			// given
-			createDataInIndex(t, client, []*Vulnerability{&aVulnerability}, 1)
+			createDataInIndex(t, client, indexName, []*Vulnerability{&aVulnerability}, 1)
 
 			// when
 			query := `{"query":{"bool":{"filter":[{"term":{"oid":{"value":"1.3.6.1.4.1.25623.1.0.117842"}}}]}}}`
@@ -158,11 +160,11 @@ func TestClient(t *testing.T) {
 			assert.Equal(t, uint(0), searchResponse.Hits.Total.Value)
 			assert.Equal(t, 0, len(searchResponse.GetResults()))
 		}},
-		"TestSearchStream": {func(t *testing.T, client *Client, _ *IndexFunction) {
+		"TestSearchStream": {func(t *testing.T, client *Client, _ *IndexFunction, indexName string) {
 			var searchResponse SearchResponse[*Vulnerability]
 
 			// given
-			createDataInIndex(t, client, []*Vulnerability{&aVulnerability}, 1)
+			createDataInIndex(t, client, indexName, []*Vulnerability{&aVulnerability}, 1)
 
 			// when
 			query := `{"query":{"bool":{"filter":[{"term":{"oid":{"value":"1.3.6.1.4.1.25623.1.0.117842"}}}]}}}`
@@ -202,13 +204,7 @@ func TestClient(t *testing.T) {
 		}},
 	}
 
-	ctx := context.Background()
-	opensearchContainer, conf, err := StartOpensearchTestContainer(ctx)
-	assert.Nil(t, err)
-	assert.NotNil(t, opensearchContainer)
-	defer func() {
-		opensearchContainer.Terminate(ctx)
-	}()
+	_, conf := getOpenSearchConfig(t)
 
 	opensearchProjectClient, err := NewOpenSearchProjectClient(context.Background(), conf, nil)
 	require.NoError(t, err)
@@ -219,19 +215,26 @@ func TestClient(t *testing.T) {
 
 	for testName, testCase := range tcs {
 		t.Run(testName, func(t *testing.T) {
-			err = iFunc.DeleteIndex(indexName)
-			if err != nil {
-				log.Trace().Msgf("Error while deleting index: %s", err)
-			}
+			t.Parallel()
+
+			indexName := "test_" + strings.ToLower(rand.Text())
+			t.Cleanup(func() {
+				err := iFunc.DeleteIndex(indexName)
+				require.NoError(t, err, "failed to delete index")
+			})
+
 			schema := folder.GetContent(t, "testdata/testSchema.json")
 			err = iFunc.CreateIndex(indexName, []byte(schema))
 			require.NoError(t, err)
-			testCase.testFunc(t, client, iFunc)
+			testCase.testFunc(t, client, iFunc, indexName)
 		})
 	}
 }
 
 func TestSerializeDocumentsForBulkUpdate(t *testing.T) {
+	t.Parallel()
+
+	indexName := "test"
 	// when
 	bulkUpdate, err := SerializeDocumentsForBulkUpdate(indexName, []*Vulnerability{&aVulnerability})
 
@@ -243,7 +246,8 @@ func TestSerializeDocumentsForBulkUpdate(t *testing.T) {
 	assert.Equal(t, expectedString, string(bulkUpdate))
 }
 
-func createDataInIndex(t *testing.T, client *Client, vulnerabilities []*Vulnerability, expectedDocumentCount uint) {
+func createDataInIndex(t *testing.T, client *Client, indexName string, vulnerabilities []*Vulnerability, expectedDocumentCount uint) {
+
 	bulkRequest, err := SerializeDocumentsForBulkUpdate(indexName, vulnerabilities)
 	require.NoError(t, err)
 
@@ -252,12 +256,12 @@ func createDataInIndex(t *testing.T, client *Client, vulnerabilities []*Vulnerab
 
 	// wait for data to be present
 	require.EventuallyWithT(t, func(c *assert.CollectT) {
-		searchResponse := searchAllVulnerabilities(c, client)
+		searchResponse := searchAllVulnerabilities(c, client, indexName)
 		assert.Equal(c, expectedDocumentCount, searchResponse.Hits.Total.Value)
 	}, 10*time.Second, 500*time.Millisecond)
 }
 
-func searchAllVulnerabilities(c *assert.CollectT, client *Client) *SearchResponse[*Vulnerability] {
+func searchAllVulnerabilities(c *assert.CollectT, client *Client, indexName string) *SearchResponse[*Vulnerability] {
 	responseBody, err := client.Search(indexName, []byte(``))
 	require.NoError(c, err)
 
